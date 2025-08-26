6672df437d0bf9d52b8270c7b7cf094954693d74
Bruna Chapelin <161771692+joyllty@users.noreply.github.com>
2025-08-26 15:09:47 -0300

    Delete Personagem Old Dragon/Personagem Old Dragon directory

--- Personagem Old Dragon/Personagem Old Dragon/racas/Gnomo.py
+++
@@ -1,9 +0,0 @@
-from racas.Raca import Raca
-
-
-class Gnomo(Raca):
-    def __init__(self):
-        super().__init__(movimento=6, infravisao=18, alinhamento='Neutro', peso_base=40)
-
-    def habilidades(self):
-        return self.habilidades['Avaliadores', 'Sagazes e Vigorosos', 'Restrições']
--- Personagem Old Dragon/Personagem Old Dragon/utils.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def limpar_tela():
-    os.system('cls')
-
-    
